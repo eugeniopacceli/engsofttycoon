e0d6ed129c7e97c1f94781ab4535b5380bb06eac
Matheus <matheusad@dcc.ufmg.br>
2017-10-09 10:37:32 -0300

    interacao entre as telas inciais e conexao dos botoes com backend

--- EngSoftTycoon/EngSoftTycoon/www/scripts/main.js
+++ EngSoftTycoon/EngSoftTycoon/www/scripts/main.js
@@ -2,17 +2,20 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // Para depurar códigos no carregamento de página em dispositivos/emuladores Android ou que simulam o Cordova: inicie o aplicativo, defina os pontos de interrupção 
 // e execute "window.location.reload()" no Console do JavaScript.
+
+var paused;
+
 (function () {
   "use strict";
   var playerName = "";
 
-  document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+  document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
   init();
 
   function init() {
     $("#content").load("startMenu.html");
-    //$("#content").load("mainPlayerScreen.html");
+    paused = false;
   }
 
   function onDeviceReady() {
@@ -29,4 +32,24 @@
   function onResume() {
     // TODO: este aplicativo foi reativado. Restaure o estado do aplicativo aqui.
   };
-} )();
+})();
+
+function start_game() {
+  $("#content").load("mainPlayerScreen.html");
+}
+
+function contratar_funcionarios() {
+  console.log("Contratando");
+}
+
+function definir_novo_projeto() {
+  console.log("Definindo novo projeto");
+}
+
+function parar_tempo() {
+  if (!paused)
+    $("#pausar_jogo_btn").addClass("ion-play").removeClass("ion-pause");
+  else
+    $("#pausar_jogo_btn").addClass("ion-pause").removeClass("ion-play");
+  paused = !paused;
+}
